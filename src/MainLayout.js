78677837fcd956405b0f043f28b3635b42f99a8e
Mcgatsby <ayanda99zulu@gmail.com>
2023-08-22 18:55:31 +0200

    added background image to nav

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
+// import  bgSidebar  from './assets/images/bg-sidebar-desktop.svg'
 
 const MainLayout = () => {
   return (
     <>
         <nav>
-            nav
+            {/* <img src={bgSidebar} alt="background art" /> */}
         </nav>
         <Outlet />
     </>
