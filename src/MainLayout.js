b0426919d841669aed77bc70cd8805cc1720a2df
Mcgatsby <ayanda99zulu@gmail.com>
2023-08-23 21:43:00 +0200

    page two structure completed

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -10,30 +10,30 @@ const MainLayout = () => {
   return (
     <div className='container'>
         <nav>
-          <div class="nav-container">
-            <div class="steps">
-              <span class={`page-number ${ activePage === "1"? 'active': ''}`}><p>1</p></span>
+          <div className="nav-container">
+            <div className="steps">
+              <span className={`page-number ${ activePage === "1"? 'active': ''}`}><p>1</p></span>
               <div className='text'>
                 <p>STEP 1</p>
                 <p className='category'>YOUR INFO</p>
               </div>
             </div>
-            <div class="steps">
-              <span class={`page-number ${ activePage === "2"? 'active': ''}`}><p>2</p></span>
+            <div className="steps">
+              <span className={`page-number ${ activePage === "2"? 'active': ''}`}><p>2</p></span>
               <div className='text'>
                 <p>STEP 2</p>
                 <p className='category'>SELECT PLAN</p>
               </div>
             </div>
-            <div class="steps">
-              <span class={`page-number ${ activePage === "3"? 'active': ''}`}><p>3</p></span>
+            <div className="steps">
+              <span className={`page-number ${ activePage === "3"? 'active': ''}`}><p>3</p></span>
               <div className='text'>
                 <p>STEP 3</p>
                 <p className='category'>ADD-ONS</p>
               </div>
             </div>
-            <div class="steps">
-              <span class={`page-number ${ activePage === "4"? 'active': ''}`}><p>4</p></span>
+            <div className="steps">
+              <span className={`page-number ${ activePage === "4"? 'active': ''}`}><p>4</p></span>
               <div className='text'>
                 <p>STEP 4</p>
                 <p className='category'>SUMMARY</p>
--- src/pages/FormPageTwo.js
+++ src/pages/FormPageTwo.js
@@ -1,14 +1,67 @@
 
-import { Link, useOutletContext  } from 'react-router-dom';
+import { useState } from 'react';
+import { Form, NavLink, useOutletContext } from 'react-router-dom';
+// Icons
+import arcade from '../assets/images/icon-arcade.svg'
+import advanced from '../assets/images/icon-advanced.svg'
+import pro from '../assets/images/icon-pro.svg'
 
 const FormPageTwo = () => {
 
   const [ setActivePage ] = useOutletContext();
 
+  const [checked, setChecked] = useState(false)
+
+
   return (
     <div className='pages pageTwo' >
-        <h1>FormPagetwo</h1>
-        <Link to='/pageThree' onClick={() => setActivePage('3')}>Next</Link>
+        <h1>Select your plan</h1>
+        <p>You have the option of monthly or yearly billing</p>
+        <Form className='form pageTwoForm'>
+        {/* add aria labels */}
+
+          <div className="arcade select">
+            <div className="content">
+              <img src={arcade} alt="arcade icon" width={30}/>
+              <section>
+                <p>Arcade</p>
+                <span>$9/mo</span>
+              </section>
+            </div>
+          </div>
+
+          <div className="advanced select">
+            <div className="content">
+              <img src={advanced} alt="advanced icon" width={30} />
+              <section>
+                <p>Advanced</p>
+                <span>$12/mo</span>
+              </section>
+            </div>
+          </div>
+
+          <div className="pro select">
+            <div className="content">
+              <img src={pro} alt="pro icon" width={30}/>
+              <section>
+                <p>Pro</p>
+                <span>$15/mo</span>
+              </section>
+            </div>
+          </div>
+
+          <div className='toggle'>
+            {/* <span className='duration'>Monthly</span> */}
+            <span className={`duration ${checked?'': 'bold'}`}>Monthly</span>
+            <label className="switch">
+              <input type="checkbox" onClick={() => setChecked(!checked)}/>
+              <span className="slider round"></span>
+            </label>
+            <span className={`duration ${checked?'bold': ''}`}>Yearly</span>
+          </div>
+        </Form>
+        <NavLink className='go-back' to='/' onClick={() => setActivePage('1')}>Go back</NavLink>
+        <NavLink className='submit' to='/pageThree' onClick={() => setActivePage('3')}>Next</NavLink>
     </div>
   )
 }
